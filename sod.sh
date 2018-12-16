094fdbe69371d77a0175e4ee41f0a7ffc6a10b30
Bezalel3639 <bezalel3639@gmail.com>
2018-12-16 20:03:28 +0300

    sod.sh as of Dec. 16, 2018: SoD (<quadruplet>) is 5184 (success)

--- sod.sh
+++ sod.sh
@@ -1,4 +1,5 @@
-echo "Git Bash version: ${BASH_VERSION}..."
+echo "Git Bash version: ${BASH_VERSION}..." >> log.txt
+echo "$(LC_ALL=en.US.utf8 date +"%b. %d, %Y %H:%M:%S")" >> log.txt
 
 function getSoD {
     linefound=$1	
@@ -6,7 +7,7 @@ function getSoD {
     subsr=${linefound:$n} #<a href='/3444'>3444</a>
     sod=`echo "$subsr" | sed 's/<\/\?[^>]\+>//g'` #OK, 3444
 
-    if [ "$sod" = 3168 ]; then
+    if [ "$sod" = 5184 ]; then
         echo "Number: $2, SoD: $sod" >> results.txt
     fi
 }
@@ -25,6 +26,10 @@ function findLine {
     done < fpage
 }
 
+function LogProgress {
+    echo "Number: $1, Time: $(LC_ALL=en.US.utf8 date +"%H:%M:%S")" >> log.txt
+}
+
 function getWebData {
     wget -O fpage "http://www.numberempire.com/$1"
 }
@@ -37,9 +42,39 @@ do
         do
             for DIGIT4 in {0..9} 
             do
-                echo "The number: $DIGIT1$DIGIT2$DIGIT3$DIGIT4"
-                getWebData "$DIGIT1$DIGIT2$DIGIT3$DIGIT4"
-                findLine "$DIGIT1$DIGIT2$DIGIT3$DIGIT4"
+                NUMBER="$DIGIT1$DIGIT2$DIGIT3$DIGIT4"
+
+                echo "The number: $NUMBER"
+                getWebData "$NUMBER"
+                findLine "$NUMBER"
+                
+                if [ "$NUMBER" = 1000 ]; then 
+                    LogProgress 1000
+                fi
+                if [ "$NUMBER" = 2000 ]; then 
+                    LogProgress 2000
+                fi
+                if [ "$NUMBER" = 3000 ]; then 
+                    LogProgress 3000
+                fi
+                if [ "$NUMBER" = 4000 ]; then 
+                    LogProgress 4000
+                fi
+                if [ "$NUMBER" = 5000 ]; then 
+                    LogProgress 5000
+                fi
+                if [ "$NUMBER" = 6000 ]; then 
+                    LogProgress 6000
+                fi
+                if [ "$NUMBER" = 7000 ]; then 
+                    LogProgress 7000
+                fi
+                if [ "$NUMBER" = 8000 ]; then 
+                    LogProgress 8000
+                fi
+                if [ "$NUMBER" = 9999 ]; then 
+                    LogProgress 9999
+                fi
             done
         done
     done
